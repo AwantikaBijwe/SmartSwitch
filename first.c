47605df5e8449519e48627b6e0672b532c5f07af
AwantikaBijwe <30284191+AwantikaBijwe@users.noreply.github.com>
2023-08-12 22:57:18 +0530

    first commit

---
+++ first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    number1=18;
+    number2=12;
+    
+    // calculate the sub
+    sum = number1 - number2;      
+    
+    printf("%d - %d = %d", number1, number2, sum);
+    printf("\ndone");
+    return 0;
+}
